ec8bb18db36b93a919c56645bb7d20971a2d9c77
Abir <elbahri.fst@uhp.ac.ma>
2024-05-25 21:02:46 +0100

    .env added

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -24,7 +24,7 @@ const Header = () => {
                 ></path>
               </svg>
             </label>
-            <Link href="/store" className="btn btn-ghost text-lg">
+            <Link href="/" className="btn btn-ghost text-lg">
               Magnitude
             </Link>
           </div>
