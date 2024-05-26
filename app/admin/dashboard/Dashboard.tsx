fa1249bc9bc143c0b2e718fc22ac799314c53bb1
iliasradouche <iliasradouche2@gmail.com>
2024-05-26 13:32:58 +0100

    Again......

--- app/admin/dashboard/Dashboard.tsx
+++ app/admin/dashboard/Dashboard.tsx
@@ -1,8 +1,8 @@
-'use client'
-import Link from 'next/link'
-import { Bar, Doughnut, Line, Pie } from 'react-chartjs-2'
-import useSWR from 'swr'
-import { formatNumber } from '@/lib/utils'
+"use client";
+import Link from "next/link";
+import { Bar, Doughnut, Line, Pie } from "react-chartjs-2";
+import useSWR from "swr";
+import { formatNumber } from "@/lib/utils";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -15,8 +15,9 @@ import {
   Legend,
   BarElement,
   ArcElement,
-} from 'chart.js'
-import Loading from '@/components/dashboardfeatures/loading/loading'
+} from "chart.js";
+import Loading from "@/components/dashboardfeatures/loading/loading";
+import { IoIosArrowDroprightCircle } from "react-icons/io";
 
 ChartJS.register(
   CategoryScale,
@@ -29,91 +30,91 @@ ChartJS.register(
   Legend,
   BarElement,
   ArcElement
-)
+);
 
 export const options = {
   responsive: true,
   plugins: {
     legend: {
-      position: 'top' as const,
+      position: "top" as const,
     },
   },
-}
+};
 
 const Dashboard = () => {
-  const { data: summary, error } = useSWR(`/api/admin/orders/summary`)
+  const { data: summary, error } = useSWR(`/api/admin/orders/summary`);
 
-  if (error) return error.message
-  if (!summary) return <Loading/>
+  if (error) return error.message;
+  if (!summary) return <Loading />;
 
   const salesData = {
     labels: summary.salesData.map((x: { _id: string }) => x._id),
     datasets: [
       {
         fill: true,
-        label: 'Sales',
+        label: "Sales",
         data: summary.salesData.map(
           (x: { totalSales: number }) => x.totalSales
         ),
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
       },
     ],
-  }
+  };
   const ordersData = {
     labels: summary.salesData.map((x: { _id: string }) => x._id),
     datasets: [
       {
         fill: true,
-        label: 'Orders',
+        label: "Orders",
         data: summary.salesData.map(
           (x: { totalOrders: number }) => x.totalOrders
         ),
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
       },
     ],
-  }
+  };
   const productsData = {
     labels: summary.productsData.map((x: { _id: string }) => x._id), // 2022/01 2022/03
     datasets: [
       {
-        label: 'Category',
+        label: "Category",
         data: summary.productsData.map(
           (x: { totalProducts: number }) => x.totalProducts
         ),
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
         ],
         borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
         ],
       },
     ],
-  }
+  };
   const usersData = {
     labels: summary.usersData.map((x: { _id: string }) => x._id), // 2022/01 2022/03
     datasets: [
       {
-        label: 'Users',
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        label: "Users",
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
         data: summary.usersData.map(
           (x: { totalUsers: number }) => x.totalUsers
         ),
       },
     ],
-  }
+  };
 
   return (
     <div className="p-4 min-h-screen">
@@ -131,21 +132,38 @@ const Dashboard = () => {
           <div className="stat-title text-gray-600 font-bold">Orders</div>
           <div className="stat-value text-blue-500">{summary.ordersCount}</div>
           <div className="stat-desc text-gray-500 hover:underline">
-            <Link href="/admin/orders">View orders</Link>
+            <Link href="/admin/orders" passHref>
+              <div className="flex items-center cursor-pointer">
+                View orders
+                <IoIosArrowDroprightCircle className="ml-1 h-5 w-5" />
+              </div>
+            </Link>
           </div>
         </div>
         <div className="stat p-4 bg-base-200 shadow-lg rounded-lg ">
           <div className="stat-title text-gray-600 font-bold">Products</div>
-          <div className="stat-value text-orange-500">{summary.productsCount}</div>
+          <div className="stat-value text-orange-500">
+            {summary.productsCount}
+          </div>
           <div className="stat-desc text-gray-500 hover:underline">
-            <Link href="/admin/products">View products</Link>
+            <Link href="/admin/products">
+              <div className="flex items-center cursor-pointer">
+                View products
+                <IoIosArrowDroprightCircle className="ml-1 h-5 w-5" />
+              </div>
+            </Link>
           </div>
         </div>
         <div className="stat p-4 bg-base-200 shadow-lg rounded-lg ">
           <div className="stat-title text-gray-600 font-bold">Users</div>
           <div className="stat-value text-red-500">{summary.usersCount}</div>
           <div className="stat-desc text-gray-500 hover:underline">
-            <Link href="/admin/users">View users</Link>
+            <Link href="/admin/users">
+              <div className="flex items-center cursor-pointer">
+                View Users
+                <IoIosArrowDroprightCircle className="ml-1 h-5 w-5" />
+              </div>
+            </Link>
           </div>
         </div>
       </div>
@@ -172,7 +190,7 @@ const Dashboard = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
